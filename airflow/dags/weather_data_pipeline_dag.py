9f91c5957b607fc4e4ec2ec9a69f25fb766de4cf
manylovmax <maximalist000@gmail.com>
2024-07-31 20:22:19 +0300

    added a DAG to download, store as a csv file and save to parquet file the weather data

--- airflow/dags/weather_data_pipeline_dag.py
+++ airflow/dags/weather_data_pipeline_dag.py
@@ -1,127 +1,83 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-"""
-### Tutorial Documentation
-Documentation that goes along with the Airflow tutorial located
-[here](https://airflow.apache.org/tutorial.html)
-"""
-
 from __future__ import annotations
 
-# [START tutorial]
-# [START import_module]
-import textwrap
-from datetime import datetime, timedelta
-
-# The DAG object; we'll need this to instantiate a DAG
-from airflow.models.dag import DAG
-
-# Operators; we need this to operate!
-from airflow.operators.bash import BashOperator
-
-# [END import_module]
 
 
-# [START instantiate_dag]
-with DAG(
-    "weather_data_pipeline_dag",
-    # [START default_args]
-    # These args will get passed on to each operator
-    # You can override them on a per-task basis during operator initialization
-    default_args={
-        "depends_on_past": False,
-        "email": ["airflow@example.com"],
-        "email_on_failure": False,
-        "email_on_retry": False,
-        "retries": 1,
-        "retry_delay": timedelta(minutes=5),
-        # 'queue': 'bash_queue',
-        # 'pool': 'backfill',
-        # 'priority_weight': 10,
-        # 'end_date': datetime(2016, 1, 1),
-        # 'wait_for_downstream': False,
-        # 'sla': timedelta(hours=2),
-        # 'execution_timeout': timedelta(seconds=300),
-        # 'on_failure_callback': some_function, # or list of functions
-        # 'on_success_callback': some_other_function, # or list of functions
-        # 'on_retry_callback': another_function, # or list of functions
-        # 'sla_miss_callback': yet_another_function, # or list of functions
-        # 'on_skipped_callback': another_function, #or list of functions
-        # 'trigger_rule': 'all_success'
-    },
-    # [END default_args]
-    description="A simple tutorial DAG",
-    schedule=timedelta(days=1),
-    start_date=datetime(2021, 1, 1),
+import urllib.request
+import pendulum
+import json
+import os
+import csv
+
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+from airflow.decorators import  dag, task
+
+JSON_WEATHER_DATA_FILE_PATH = os.getcwd() + '/tmp.json'
+CSV_FILE_PATH = os.getcwd() + '/processed_weather_data.csv'
+
+
+@dag(
+    schedule='@once',
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    tags=["example"],
-) as dag:
-    # [END instantiate_dag]
+    tags=["weather", "test"],
+)
+def weather_data_pipeline_dag():
 
-    # t1, t2 and t3 are examples of tasks created by instantiating operators
-    # [START basic_task]
-    t1 = BashOperator(
-        task_id="print_date",
-        bash_command="date",
-    )
+    @task(task_id='download_data')
+    def download_data_callable():
+        with urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=London&appid=2ccd190a185aa973681572f0e6d1b9e0') as file:
+            with open(JSON_WEATHER_DATA_FILE_PATH, "w") as new_file:
+                bytes = file.read()
+                new_file.write(bytes.decode())
 
-    t2 = BashOperator(
-        task_id="sleep",
-        depends_on_past=False,
-        bash_command="sleep 5",
-        retries=3,
-    )
-    # [END basic_task]
+    download_data = download_data_callable()
 
-    # [START documentation]
-    t1.doc_md = textwrap.dedent(
-        """\
-    #### Task Documentation
-    You can document your task using the attributes `doc_md` (markdown),
-    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
-    rendered in the UI's Task Instance Details page.
-    ![img](https://imgs.xkcd.com/comics/fixing_problems.png)
-    **Image Credit:** Randall Munroe, [XKCD](https://xkcd.com/license.html)
-    """
-    )
+    @task(task_id='process_data')
+    def process_data_callable():
+        def kelvin_to_celsius(kelvin):
+             return kelvin - 273.15
+        
+        def append_data_to_csv(data):
+            exists = os.path.isfile(CSV_FILE_PATH)
+            if exists:
+                with open(CSV_FILE_PATH, 'a') as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    csv_writer.writerow(data.values())
+            else:
+                with open(CSV_FILE_PATH, 'w') as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    csv_writer.writerow(data.keys())
+                    csv_writer.writerow(data.values())
 
-    dag.doc_md = __doc__  # providing that you have a docstring at the beginning of the DAG; OR
-    dag.doc_md = """
-    This is a documentation placed anywhere
-    """  # otherwise, type it like this
-    # [END documentation]
+             
 
-    # [START jinja_template]
-    templated_command = textwrap.dedent(
-        """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-    {% endfor %}
-    """
-    )
+        with open(JSON_WEATHER_DATA_FILE_PATH, "r") as json_file:
+                json_data = json.load(json_file)
+        
+        json_data['main']['temp'] = kelvin_to_celsius(json_data['main']['temp'])
+        json_data['main']['feels_like'] = kelvin_to_celsius(json_data['main']['feels_like'])
+        json_data['main']['temp_min'] = kelvin_to_celsius(json_data['main']['temp_min'])
+        json_data['main']['temp_max'] = kelvin_to_celsius(json_data['main']['temp_max'])
+        
+        append_data_to_csv(json_data)
 
-    t3 = BashOperator(
-        task_id="templated",
-        depends_on_past=False,
-        bash_command=templated_command,
-    )
-    # [END jinja_template]
 
-    t1 >> [t2, t3]
-# [END tutorial]
+
+    process_data = process_data_callable()
+
+    @task(task_id='save_data')
+    def save_data_callable():
+        df = pd.read_csv(CSV_FILE_PATH)
+        table = pa.Table.from_pandas(df)
+        parquet_file_path = os.getcwd() + '/weather.parquet'
+        pq.write_table(table, parquet_file_path)
+            
+
+    save_data = save_data_callable()
+
+    download_data >> process_data >> save_data
+
+weather_data_pipeline_dag()
